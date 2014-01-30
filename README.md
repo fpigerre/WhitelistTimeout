4cd28dee0899fb240f41ed982f836ece9b87074d
psgs <fpigerre@gmail.com>
2014-01-30 19:44:43 +1000

    Add Travis Support

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-Whitelist Timeout
-=================
+Whitelist Timeout [![Build Status](https://travis-ci.org/psgs/WhitelistTimeout.png?branch=master)](https://travis-ci.org/psgs/WhitelistTimeout)
+=======================
 
 Whitelist Timeout is a bukkit plugin that removes players from a server whitelist if they have been inactive for more than fourteen days.
 
--- src/main/java/io/github/psgs/whitelisttimeout/WhitelistTimeout.java
+++ src/main/java/io/github/psgs/whitelisttimeout/WhitelistTimeout.java
@@ -1,36 +1,40 @@
 package io.github.psgs.whitelisttimeout;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 
 public class WhitelistTimeout extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        System.out.println("Removing inactive whitelisted players!");
-        Set<OfflinePlayer> whiteListedPlayers = Bukkit.getWhitelistedPlayers();
-        for (OfflinePlayer player : whiteListedPlayers) {
-            if (System.currentTimeMillis() - player.getLastPlayed() > 1209600000) {
-                if (player.getPlayer().hasPermission("mapdev.staff")) {
-                    return;
-                } else {
-                    player.setWhitelisted(false);
-                }
-            }
-        }
-        System.out.println("Inactive whitelisted players removed!");
-        System.out.println("Disabling plugin!");
-        getServer().getPluginManager().disablePlugin(this);
+
     }
 
     @Override
     public void onDisable() {
 
+    }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("whitelist")) {
+            if (args[0].equalsIgnoreCase("timeout")) {
+                Set<OfflinePlayer> whiteListedPlayers = Bukkit.getWhitelistedPlayers();
+                for (OfflinePlayer player : whiteListedPlayers) {
+                    if (System.currentTimeMillis() - player.getLastPlayed() > 1209600000) {
+                        if (!player.getPlayer().hasPermission("mapdev.staff")) {
+                            player.setWhitelisted(false);
+                        }
+                    }
+                }
+                sender.sendMessage(ChatColor.GREEN + "Inactive whitelisted players removed!");
+            }
+        }
+        return false;
     }
 }
